113be9c62358a5b81f6d5ba2127280527db30cfd
Hold <95875067+HoldCodes@users.noreply.github.com>
2022-03-28 20:55:52 -0300

    Add files via upload

--- testes/main.py
+++ testes/main.py
@@ -2,6 +2,9 @@ import numpy as np
 from pkg.mapa import mapa
 from pkg.robo import Robo
 
+# Pular uma linha no depois da ultima vitima
+# Se for mudar o ambiente mudar o limite do mapa no config.txt 
+
 
 def formata(linha):
     formLine = []
@@ -20,13 +23,16 @@ def formata(linha):
 
 def cria_mapa(lim_x, lim_y):
 
-    arq = open("data/ambiente.txt", "r")
+    #arq = open("data/ambiente.txt", "r")
+    arq = open("data/ambiente2.txt", "r")
     lines = arq.readlines()
 
     agent = formata(lines[0])
     goal = formata(lines[1])
     paredes = formata(lines[2])
     vitimas = formata(lines[3])
+
+    print(vitimas)
 
     tabuleiro = np.zeros((int(lim_x), int(lim_y)), dtype=str)
 
@@ -45,7 +51,6 @@ def cria_mapa(lim_x, lim_y):
         tabuleiro[int(vitimas[x])][int(vitimas[x+1])] = 'V'
 
     return tabuleiro
-
 
 
 def main():
